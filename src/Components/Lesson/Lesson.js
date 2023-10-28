c780819f3645faa619398a27dc7eb633f9675b28
Alina <palinchuk2016@gmail.com>
2023-10-28 15:37:47 +0300

    changed colors for practice and lecture

--- src/Components/Lesson/Lesson.js
+++ src/Components/Lesson/Lesson.js
@@ -1,7 +1,8 @@
 import styles from './Lesson.module.css'
 
 const Lesson = ({lessonInfo}) => {
-    return <div className={styles.lesson_wrapper}>
+    console.log('fjkdlsajf', lessonInfo)
+    return <div className={`${styles.lesson_wrapper} ${lessonInfo.subject.type === 'Practice' ? styles.lessonPractice : styles.lessonLecture}`}>
         <p className={styles.lesson_title}>{lessonInfo.subject.name}</p>
         <p className={styles.lesson_room}>кабінет {lessonInfo.room.number}</p>
     </div>
